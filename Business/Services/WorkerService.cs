966bc5adf4fad58f18e179cd59c5c5bb1503d9d4
nrnhh <113415460+nrnhh@users.noreply.github.com>
2022-10-22 22:12:24 +0400

    bug-fix , added new methods for Workers

--- Business/Services/WorkerService.cs
+++ Business/Services/WorkerService.cs
@@ -1,46 +1,127 @@
 ﻿using Business.Interfaces;
+using DataAccess.Repositories;
 using Entities.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
 
 namespace Business.Services
 {
     public class WorkerService : IWorker
     {
+        public CompanyService companyService { get; set; }
+        public WorkerRepository workerRepository { get; set; }
+        public static int Count { get; set; }
+        public WorkerService()
+        {
+            companyService = new CompanyService();  
+            workerRepository = new WorkerRepository();  
+        }
         public Worker Create(Worker worker, string groupName)
         {
-            throw new NotImplementedException();
+           Company company = companyService.Get(groupName);
+            if (company!=null)
+            {
+                worker.Id = Count;
+                worker.Company = company;   
+                workerRepository.Add(worker);
+                Count++;
+                return worker;
+            }
+           
+            
+                return null;
+            
         }
 
         public Worker Delete(int Id)
         {
-            throw new NotImplementedException();
+            Worker existWorker = workerRepository.Get(w => w.Id == Id);
+            if (existWorker != null)
+            {
+                workerRepository.Remove(existWorker);
+                return existWorker;
+            }
+            else
+            {
+                return null;
+            }
         }
 
-        public Worker Get(int Id)
+        public Worker Get(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Worker worker = workerRepository.Get(w => w.Id == id);
+                if (worker != null)
+                {
+                    return worker;
+                }
+                return null;
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
         }
 
         public List<Worker> Get(string companyName)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Company company = companyService.Get(companyName);
+                if (company!=null)
+                {
+                    List<Worker> workers = workerRepository.GetAll(w=>w.Company.Name.ToLower()==company.Name.ToLower());
+                    return workers;
+                }
+                return null;
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
         }
 
         public List<Worker> GetAll(string name)
         {
-            throw new NotImplementedException();
+            return workerRepository.GetAll(w => w.Name.ToLower() == name.ToLower());
         }
 
         public List<Worker> GetAll()
         {
-            throw new NotImplementedException();
+          return workerRepository.GetAll(); 
         }
 
-        public Worker Update(Worker worker, string groupName)
+        public Worker Update(Worker worker, string workerName)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Worker existWorker = workerRepository.Get(w => w.Name.ToLower() ==worker.Name.ToLower() );
+
+
+                if (existWorker != null)
+                {
+                    existWorker.Name = worker.Name;
+                  
+
+                    return existWorker;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
+
+        
     }
 }
